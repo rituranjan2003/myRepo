a3a218bf56f1fab6cbc6dbff1eb11604bb541b25
Rritu Ranjan <rituranjan2003@gmail.com>
2018-08-03 10:01:16 -0500

    first commit

---
+++ src/classes/mathFunctions.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class mathFunctions {
+static int a,b,c,total;
+
+public static int addition( int a, int b)
+{
+	total= a+b;
+	return total;
+}
+
+public static int substraction(int a, int b)
+{
+	total=a-b;
+	return total;
+}
+
+public static int multiplication(int a, int b)
+{
+	total=a*b;
+	return total;
+}
+
+}
